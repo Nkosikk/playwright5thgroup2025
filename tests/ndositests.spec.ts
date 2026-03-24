63704c76fe7481e9927faec7162f065d397d14a6
Nkosi <njceles@gmail.com>
2026-03-24 20:42:29 +0200

    first commit

---
+++ tests/ndositests.spec.ts
@@ -0,0 +1,28 @@
+import {test,expect} from '@playwright/test'
+
+
+test("verify ndosi automation title",async({page})=>{
+
+    await page.goto("https://ndosisimplifiedautomation.vercel.app/#")
+
+    await expect(page).toHaveTitle("Ndosi Test Automation")
+})
+
+
+test("login to mdosi website",async({page})=>{
+
+    await page.goto("https://ndosisimplifiedautomation.vercel.app/#")
+
+    //  await page.getByRole('button', { name: 'Login' }).click();
+      await page.click('text=Login')
+     await page.waitForTimeout(3000)
+
+  await page.getByRole('textbox', { name: 'Email' }).fill('admin@gmail.com')
+  await page.getByRole('textbox', { name: 'Password' }).fill('@12345678')
+  await page.waitForTimeout(3000)
+    await page.getByRole('button', { name: 'Login' }).nth(1).click()
+  await page.waitForTimeout(5000)
+
+ 
+})
+
