d9432e017beb55d3391d9bbad2fbee1eb85da432
Spencer Soundy <spencersoundy@gmail.com>
2026-03-31 19:33:56 +0200

    created a click review and enter title test

--- tests/ndositests.spec.ts
+++ tests/ndositests.spec.ts
@@ -23,13 +23,12 @@ test("login to mdosi website",async({page})=>{
     await page.getByRole('button', { name: 'Login' }).click()
   await page.waitForTimeout(5000)
 
- 
-})
-test("click write review and enter title",async({page})=>{
-
-    await page.locator('button').filter({ hasText: '+ Add Review' }).click();
+  await page.locator('button').filter({ hasText: '+ Add Review' }).click();
     await page.waitForTimeout(3000)
     await page.getByRole('textbox', { name: 'E.g., Great Learning Experience!' }).fill('This is a review for the product')
     await page.waitForTimeout(3000)
+
+ 
 })
+
 
